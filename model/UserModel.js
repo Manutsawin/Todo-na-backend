6ad8a492844dd48674edd5d009d3e6f7817eb18f
SK-Pupae <SK-Pupae@DESKTOP-BECBDNN>
2021-04-18 02:33:19 +0700

    test

--- model/UserModel.js
+++ model/UserModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     Name: String,
-    UserID: String,
+    UserID: {type: String ,unique: true},
     Password: String,
     Role : String,  
 });
