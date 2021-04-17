c045d68bfd698c0c4639c96d58ad76b207436243
SK-Pupae <SK-Pupae@DESKTOP-BECBDNN>
2021-04-18 01:29:49 +0700

    test

--- services/updateUserService.js
+++ services/updateUserService.js
@@ -11,6 +11,7 @@ module.exports = async function updateUserService(req,res){
     if(decoded.role=="Admin")
     {
         const user = await UserModel.findOneAndUpdate({_id : req.query.id },req.body);
+        res.send(200);
     }
     else{
         res.send("not Admin");
